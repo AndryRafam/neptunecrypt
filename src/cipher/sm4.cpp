1094e239444dd51392a907569220dd0d9cde3cee
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2026-07-06 18:35:12 +0300

    Update sm4.cpp

--- src/cipher/sm4.cpp
+++ src/cipher/sm4.cpp
@@ -1,141 +1,144 @@
 #include <iostream>
 
-#include "xchacha20.hpp"
+#include "sm4.hpp"
 
 using namespace CryptoPP;
 
-bool xchacha20_cipher(std::string mode, std::string filePath, std::string password) {
-	
-	// number of threads
+bool sm4_cipher(std::string mode, std::string filePath, std::string password) {
+
+    // number of threads
 	const int threads = 4;
 
-	// temporary file to avoid
-	// data loss
-	std::string tempfile = filePath+".tmp";
-	std::string tempfile_hex = filePath+".tmphex"; // temporary file for hexadecimal encoding
-	
-	const int XCHACHA20_KEY_SIZE = 32; // key length 32 bytes
-	const int XCHACHA20_NONCE_SIZE = 24; // nonce length 24 bytes
-	const int TAG_SIZE = 16; // tag size 16 bytes
-	const int SALT_SIZE = 16; // Argon2 recommends at least 16 bytes
+    // temporary file to avoid
+    // data loss
+    std::string tempfile = filePath+".tmp";
+    std::string tempfile_hex = filePath+".tmphex";
 
-	try {
-		
-		AutoSeededRandomPool rng;
-		
-		/*ENCRYPTION MODE*/
-		if(mode=="encrypt") {
-			SecByteBlock key(XCHACHA20_KEY_SIZE);
-			rng.GenerateBlock(key, key.size());
+    const int IV_SIZE = 12; // iv size
+    const int TAG_SIZE = 16; // standard 128-bit authentication tag
+    const int SALT_SIZE = 16; // Argon2 recommends at least 16 bytes
 
-			SecByteBlock nonce(XCHACHA20_NONCE_SIZE);
-			rng.GenerateBlock(nonce, nonce.size());
+    try {
 
-			SecByteBlock salt(SALT_SIZE);
-			rng.GenerateBlock(salt, salt.size());
+        AutoSeededRandomPool rng;
+        
+        /*ENCRYPTION MODE*/
+        if(mode=="encrypt") {
+        
+            // Setup key, IV and unique random salt
+            SecByteBlock key(SM4::DEFAULT_KEYLENGTH); // 16 bytes (128 bits)
+            rng.GenerateBlock(key, key.size());
+            
+            SecByteBlock iv(IV_SIZE); // 12 bytes (96 bits)
+            rng.GenerateBlock(iv, iv.size());
 
-			// derive key using Argon2id
-			Argon2 argon2(Argon2::ARGON2ID);
-			argon2.DeriveKey(key, key.size(),
-				(const byte*)password.data(), password.size(),
-				salt.data(), salt.size(),
-				3, // time cost (iterations)
-				65536, // memory cost (64 MB)
-				threads // number of threads
-			);
+            SecByteBlock salt(SALT_SIZE);
+            rng.GenerateBlock(salt, salt.size());
 
-			//write Salt + IV + Ciphertext into a temporary binary file
-			{
-				FileSink binarySink(tempfile.c_str());
+            // derive key using Argon2id
+            Argon2 argon2(Argon2::ARGON2ID);
+            argon2.DeriveKey(key, key.size(),
+                (const byte*)password.data(), password.size(),
+                salt.data(), salt.size(),
+                3, // time cost (iterations)
+                65536, // memory cost (64 MB)
+                threads // number of threads
+            );
+            
+            {
+                FileSink binarySink(tempfile.c_str());
 
-				// writer cipher ID for XChaCha20Poly1305
-				byte cipherID = 0x04;
-				binarySink.Put(&cipherID, 1);
+                // write cipher ID for SM4
+                byte cipherID = 0x02;
+                binarySink.Put(&cipherID, 1);
 
-				binarySink.Put(salt, salt.size());
-				binarySink.Put(nonce, nonce.size());
+                // write Salt + IV + Ciphertext into a temporary binary file
+                binarySink.Put(salt, salt.size());
+                binarySink.Put(iv, iv.size());
 
-				XChaCha20Poly1305::Encryption xchacha_enc;
-				xchacha_enc.SetKeyWithIV(key, key.size(), nonce, nonce.size());
+                GCM<SM4>::Encryption sm4_enc;
+                sm4_enc.SetKeyWithIV(key, key.size(), iv, iv.size());
 
-				// stream the file payload right after the header data
-				FileSource(filePath.c_str(), true,
-					new AuthenticatedEncryptionFilter(xchacha_enc, new Redirector(binarySink), false, TAG_SIZE)
-				);
-			}
-		
-			// hexadecimal encoding
-			// for pretty looking
-			FileSource(tempfile.c_str(), true, new HexEncoder(new FileSink(tempfile_hex.c_str())));
-			
-			// cleanup and swap files
-			std::remove(filePath.c_str()); // remove the former filePath
-			std::remove(tempfile.c_str()); // remove the tempfile
-			std::rename(tempfile_hex.c_str(), filePath.c_str()); // rename the hexadecimal encrypted file
-			
-			return true;
-		}
-		/*DECRYPTION MODE*/
-		else {
-			
-			// hexadecimal decoding
-			FileSource(filePath.c_str(), true, new HexDecoder(new FileSink(tempfile_hex.c_str())));
-			
-			// extract salt and IV out of the decoded temporary file
+                // stream the file payload right after the header data
+                FileSource(filePath.c_str(), true,
+                    new AuthenticatedEncryptionFilter(sm4_enc, new Redirector(binarySink), false, TAG_SIZE)
+                );
+            }
+            
+            // hexadecimal encoding
+            // for pretty looking
+            FileSource(tempfile.c_str(), true, new HexEncoder(new FileSink(tempfile_hex.c_str())));
+
+            // cleanup and swap files
+            std::remove(filePath.c_str());
+            std::remove(tempfile.c_str());
+            std::rename(tempfile_hex.c_str(), filePath.c_str());
+
+            return true;
+        }
+        /*DECRYPTION MODE*/
+        else {
+
+            // hexadecimal decoding
+            FileSource(filePath.c_str(), true, new HexDecoder(new FileSink(tempfile_hex.c_str())));
+
+            // extract salt and IV out of the decoded temporary file
             std::ifstream in(tempfile_hex.c_str(), std::ios::binary);
 
-			// extract and validate cipher ID
-			byte cipherID = 0;
-			in.read((char*)&cipherID, 1);
-			if (in.gcount()!=1) throw std::runtime_error("File truncated: Missing Cipher ID.");
-			if (cipherID != 0x04) throw std::runtime_error("Cipher ID mismatch: This file was not encrypted with XChaCha20Poly1305");
-			
-			SecByteBlock salt(SALT_SIZE);
-			in.read((char*)salt.data(), salt.size());
-			if (in.gcount() != SALT_SIZE) throw std::runtime_error("File truncated: Missing salt.");
+            // extract and validate cipher ID
+            byte cipherID = 0;
+            in.read((char*)&cipherID, 1);
+            if (in.gcount()!=1) throw std::runtime_error("File truncated: Missing Cipher ID.");
+            if (cipherID != 0x02) throw std::runtime_error("Cipher ID mismatch: This file was not encrypted with SM4.");
 
-			SecByteBlock nonce(XCHACHA20_NONCE_SIZE);
-			in.read((char*)nonce.data(), nonce.size());
-			if (in.gcount() != XCHACHA20_NONCE_SIZE) throw std::runtime_error("File truncated: Missing NONCE.");
+            
+            SecByteBlock salt(SALT_SIZE);
+            in.read((char*)salt.data(), salt.size());
+            if (in.gcount() != SALT_SIZE) throw std::runtime_error("File truncated: Missing salt.");
 
-			SecByteBlock key(XCHACHA20_KEY_SIZE);
-			Argon2 argon2(Argon2::ARGON2ID);
-			argon2.DeriveKey(
-				key, key.size(),
-				(const byte*)password.data(), password.size(),
-				salt, salt.size(),
-				3, 65536, threads // must exactly match encryption parameters
-			);
+            SecByteBlock iv(IV_SIZE);
+            in.read((char*)iv.data(), iv.size());
+            if (in.gcount() != IV_SIZE) throw std::runtime_error("File truncated: Missing IV.");
 
-			// decryption object
-			XChaCha20Poly1305::Decryption xchacha_dec;
-			xchacha_dec.SetKeyWithIV(key, key.size(), nonce, nonce.size());
-			
-			AuthenticatedDecryptionFilter df(xchacha_dec, new FileSink(tempfile.c_str()));
-			FileSource(in, true, new Redirector(df));
-			
-			if(true==df.GetLastResult()) {
-				in.close(); // release file handle before deleting
-				std::remove(filePath.c_str()); // remove the former filePath
-				std::remove(tempfile_hex.c_str()); // remove the intermediate file
-				std::rename(tempfile.c_str(), filePath.c_str()); // rename the decrypted file
-				return true;
-			} else {
-				throw::std::runtime_error("\nAuthentication failed.\n");
-			}
-		}
-	}
-	
-	catch(Exception& ex) {
-		std::cout << "\033[1;31m" << "\nError: Wrong password or Corrupted data.\n";
+            // re derive the exact key using the extracted salt
+            SecByteBlock key(SM4::DEFAULT_KEYLENGTH);
+            Argon2 argon2(Argon2::ARGON2ID);
+            argon2.DeriveKey(
+                key, key.size(),
+                (const byte*)password.data(), password.size(),
+                salt, salt.size(),
+                3, 65536, threads // must exactly match encryption parameters
+            );
+
+            // decryption object
+            GCM<SM4>::Decryption sm4_dec;
+            sm4_dec.SetKeyWithIV(key, key.size(), iv, iv.size());
+            
+            AuthenticatedDecryptionFilter df(sm4_dec, new FileSink(tempfile.c_str()));
+            FileSource(in, true, new Redirector(df));
+
+            if(true==df.GetLastResult()) {
+                in.close(); // release file handle before deleting
+                std::remove(filePath.c_str());
+                std::remove(tempfile_hex.c_str());
+                std::rename(tempfile.c_str(), filePath.c_str());
+                return true;
+            } else {
+                throw::std::runtime_error("\nAuthentication failed.\n");
+            }
+        }
+    }
+
+    catch(Exception& ex) {
+        std::cout << "\033[1;31m" << "\nError: Wrong password or Corrupted data.\n";
 		std::cout << ex.what() << "\n";
 		std::cout << "Cannot decrypt." << "\033[0m" << "\n\n";
-		
-		// remove the temporary file even decryption failed.
-		std::remove(tempfile.c_str());
-		std::remove(tempfile_hex.c_str());
-		
-		return false;
-	}
+
+        // remove the tempfile even if decryption failed
+        std::remove(tempfile.c_str());
+        std::remove(tempfile_hex.c_str());
+
+        return false;
+    }
 }
 
